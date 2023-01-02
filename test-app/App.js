13e38f9209ee937d5f729f3957f8688357ba688e
Lipsia Digital Dev Mac <development@matchwerk.de>
2023-01-02 19:55:45 +0100

    cleanup

--- test-app/App.js
+++ test-app/App.js
@@ -10,10 +10,11 @@ export default function App() {
     <View style={{ width: "100%", height: "100%" }}>
       {Boolean(view === 0) && <WebView style={{ width: "100%", height: 300, backgroundColor: "red" }} cacheEnabled={true} cacheMode='LOAD_DEFAULT' source={{ uri: 'https://pwa-demo.glitch.me' }} />}
       {Boolean(view === 1) && <WebView style={{ width: "100%", height: 300, backgroundColor: "red" }} cacheEnabled={true} cacheMode='LOAD_DEFAULT' source={{ uri: 'https://pwa.deimel.dev' }} />}
+      {Boolean(view === 2) && <WebView style={{ width: "100%", height: 300, backgroundColor: "red" }} cacheEnabled={true} cacheMode='LOAD_DEFAULT' source={{ uri: 'https://leoek.github.io/pwa-app-test' }} />}
       <Button 
         title="next"
         onPress={() => {
-        if (view < 1){
+        if (view < 2){
           setView(view + 1);
         } else {
           setView(0)
